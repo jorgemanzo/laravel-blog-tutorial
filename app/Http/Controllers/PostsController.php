3deff2a4435dde1da288a518bb188c8a71277cec
jorgemanzo <jmanzo203689@gmail.com>
2020-07-12 19:12:32 -0700

    Switched from compact to array literal

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,42 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-
 use App\Post;
 
 class PostsController extends Controller
 {
-
     private $active = 'home';
 
     public function index()
     {
         $posts = Post::latest()->get();
-        return view('posts/index', ['posts' => $posts, 'active' => $this->active]);
+        return view('posts/index', [
+            'posts' => $posts, 
+            'active' => $this->active
+        ]);
     }
 
     public function create()
     {
-        $active = 'create';
-        return view('posts/create', compact('active'));
+        return view('posts/create', ['active' => $this->active]);
     }
 
     public function store()
     {
-
         $this->validate(request(), [
             'title' => 'required',
             'body' => 'required'
         ]);
-
         $post = Post::create(request(['title', 'body']));
-
         return redirect()->route('post', ['post' => $post->id]);
-        
     }
 
     public function show(Post $post)
     {
-        return view('posts/show', ['post' => $post, 'active' => $this->active]);
+        return view('posts/show', [
+            'post' => $post, 
+            'active' => $this->active
+        ]);
     }
 }
