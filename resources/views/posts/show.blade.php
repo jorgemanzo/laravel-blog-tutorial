49838b5e6781d13cb247915fd4e0fb0bd1362df8
jorgemanzo <jmanzo203689@gmail.com>
2020-07-13 20:49:12 -0700

    Show comments under post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,6 +13,21 @@
         <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
 
         <p>{{ $post->body }}</p>
+        <hr/>
+        <div class="comments">
+            <ul class="list-group">
+            @foreach ($post->comments as $comment)
+                <article>
+                    <li class="list-group-item">
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                        </strong>
+                        {{ $comment->body }}
+                    </li>
+                </article>
+            @endforeach
+            </ul>
+        </div>
 
     </div><!-- /.blog-post -->
 
