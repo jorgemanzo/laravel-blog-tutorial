c7dada9c56332ad66c9f5b0f993345b16dde6a5a
jorgemanzo <jmanzo203689@gmail.com>
2020-07-13 21:16:41 -0700

    Adds a form to create comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -29,6 +29,22 @@
             </ul>
         </div>
 
+        {{-- Adda a comment --}}
+        <hr/>
+        <div class="card">
+            <div class="card-block">
+                <form method="POST" action="/posts/{{ $post->id }}/comments" >
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here...." class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+                @include('layouts.errors')
+            </div>
+        </div>
     </div><!-- /.blog-post -->
 
 @endsection
